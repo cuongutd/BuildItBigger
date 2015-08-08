da765c36843712e0b3b1c515d89c1f073ed9b557
cuongutd <cuongutd@gmail.com>
2015-08-08 11:51:29 -0700

    removed google ad service dependency from paid flavor moved gradle task to root build.gradle

--- app/src/paid/java/com/cuong/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/cuong/gradle/builditbigger/MainActivityFragment.java
@@ -1,14 +1,9 @@
 package com.cuong.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 
 /**
  * A placeholder fragment containing a simple view.
